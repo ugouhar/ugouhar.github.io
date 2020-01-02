a29a52a9d470f93e645b86ee542cb5057416e946
Ushran Gouhar <43915259+gouhar1512@users.noreply.github.com>
2020-01-02 22:26:08 +0530

    Add files via upload

--- js/transition.js
+++ js/transition.js
@@ -18,7 +18,10 @@ function isVisible(elem) {
   var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
   var top_of_screen = $(window).scrollTop();
 
-  if (bottom_of_screen > top_of_element && top_of_screen < bottom_of_element) {
+  if (
+    bottom_of_screen + 50 > top_of_element &&
+    top_of_screen < bottom_of_element
+  ) {
     return true;
   } else {
     return false;
